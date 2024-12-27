6484d6a2311b9af3153307d5872de6fa28fb3659
Qianban2027 <qianban2027@outlook.com>
2024-12-27 14:39:24 +0800

    committed by git_sync.bat

--- Code/git_sync.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-git pull origin main
-git add .
-git commit -m "committed by git_sync.sh"
-git push origin main
-echo "Git sync complete!"
--- Fuck_Disk/fuck.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-copy fuckE.bat e:
-copy fuckF.bat f:
-e:
-start "fuckE" cmd /k call fuckE.bat
-f:
-start "fuckF" cmd /k call fuckF.bat
